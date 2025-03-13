ae2758699092db67a2cdf029683765ced6bbcaea
Wahyu NT <wahyuntx28@gmail.com>
2025-03-13 15:36:24 +0700

    Refactor user migration and add business trip and city migrations

---
+++ database/migrations/2025_03_13_082600_create_business_trip_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_trip', function (Blueprint $table) {
+            $table->id();
+            $table->string('city_name');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('province');
+            $table->string('island')->nullable();
+            $table->boolean('is_abroad')->default(false);
+            $table->string('country')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_trip');
+    }
+};
---
+++ database/migrations/2025_03_13_082614_create_city_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('city', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('purpose_destination');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->foreignId('origin_city_id')->constrained('city')->onDelete('cascade');
+            $table->foreignId('destination_city_id')->constrained('city')->onDelete('cascade');
+            $table->integer('trip_duration')->nullable();
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->integer('total_allowance')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('city');
+    }
+};
