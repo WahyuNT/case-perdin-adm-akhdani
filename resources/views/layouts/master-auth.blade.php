9dca8c1d6000ba8bc7a9b78e5e93f116dfcf5709
Wahyu NT <wahyuntx28@gmail.com>
2025-03-17 05:14:55 +0700

    make readme

--- resources/views/layouts/master-auth.blade.php
+++ resources/views/layouts/master-auth.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('images/icon_ars.ico') }}">
     {{-- <script src="https://unpkg.com/@tailwindcss/browser@4"></script> --}}
     <script src="{{ asset('js/tailwind.js') }}"></script>
-    @vite(['resource/css/app.css', 'resource/js/app.js'])
+    {{-- @vite(['resource/css/app.css', 'resource/js/app.js']) --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <title>Perdin - @yield('title')</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="icon" type="image/x-icon" href="{{ asset('images/icon_ars.ico') }}">
-    @vite(['resource/css/app.css', 'resource/js/app.js'])
+    {{-- @vite(['resource/css/app.css', 'resource/js/app.js']) --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
